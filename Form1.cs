71ad0fe3d892977ced3dfca1c9d229b52ae045ef
hee082024kh <hee082024kh@gmail.com>
2025-01-17 10:25:04 +0100

    first

--- Form1.cs
+++ Form1.cs
@@ -9,13 +9,27 @@ public partial class Form1 : Form
     }
 
     private Bitmap canvasBitmap;
-    private Graphics graphics;
+    private Graphics canvasGraphics;
     int canvasWidth = 15, canvasHeight = 20;
     private int[,] canvasDotArray;
     private int dotSize = 20;
-
+    private PictureBox pictureBox1;
+    
     private void loadCanvas()
     {
+        // Resize the picture box based on the dotsize and canvas size
         pictureBox1.Width = canvasWidth * dotSize;
+        pictureBox1.Height = canvasHeight * dotSize;
+        
+        // Create Bitmap with picture box's size
+        canvasBitmap = new Bitmap(canvasWidth, canvasHeight);
+        canvasGraphics = Graphics.FromImage(canvasBitmap);
+        canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasWidth, canvasHeight);
+        
+        // Load bitmap into picture box
+        pictureBox1.Image = canvasBitmap;
+        
+        // Initialize canvas dot array. by default all elements are zero
+        canvasDotArray = new int[canvasWidth, canvasHeight];
     }
 }
